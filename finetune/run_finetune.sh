01332070a27b40e416493c35e1e36ac5c60cdd89
Michael Clifford <mcliffor@redhat.com>
2023-12-12 17:08:40 -0500

    update finetune dir

--- finetune/run_finetune.sh
+++ finetune/run_finetune.sh
@@ -6,11 +6,18 @@ llama.cpp/finetune \
     --checkpoint-out chk-lora-llama-7b-chat-Q5-shakespeare-ITERATION.gguf \
     --lora-out lora-llama-7b-chat-Q5-shakespeare-ITERATION.bin \
     --train-data $DATA \
-    --save-every 2 \
-    --threads 10 --adam-iter 2 --batch 4 --ctx 64 \
+    --save-every 10 \
+    --threads 10 --adam-iter 10 --batch 4 --ctx 64 \
     --use-checkpointing \
 
 llama.cpp/export-lora \
     --model-base $MODEL_FILE \
     --model-out $NEW_MODEL \
     --lora-scaled lora-llama-7b-chat-Q5-shakespeare-LATEST.bin 1.0
+
+echo "Fine Tuning Complete... 
+Please move trained model to desired location 
+
+Example: 'podman cp <container name>:<model_file> .' from the host machine" 
+
+bash
