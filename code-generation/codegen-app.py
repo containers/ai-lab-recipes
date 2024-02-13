dfe9a3401463ba121ff1832f6e6b2f57e6057ef9
Surya Prakash Pathak <supathak@redhat.com>
2024-02-13 10:42:43 -0800

    Updated prompt

--- code-generation/codegen-app.py
+++ code-generation/codegen-app.py
@@ -24,7 +24,7 @@ llm = ChatOpenAI(base_url=model_service,
 # Define the Langchain chain
 prompt = ChatPromptTemplate.from_template("""You are an helpful code assistant that can help developer to code for a given {input}. 
                                           Generate the code block at first, and explain the code at the end.
-                                          If the {input} is not making sense, please ask more clarification.""")
+                                          If the {input} is not making sense, please ask for more clarification.""")
 chain = (
     {"input": RunnablePassthrough()}
     | prompt
