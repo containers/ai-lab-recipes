24d70c045b58c808758525fd2d5bdd6c9ed3cf8d
Surya Prakash Pathak <supathak@redhat.com>
2024-02-13 10:38:54 -0800

    Addressed PR feedback

--- code-generation/codegen-app.py
+++ code-generation/codegen-app.py
@@ -1,14 +1,12 @@
+import os
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
+from langchain_community.callbacks import StreamlitCallbackHandler 
 
 import streamlit as st
 
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--model_url", default="http://0.0.0.0:8001/v1")
-args = parser.parse_args()
+model_service = os.getenv("MODEL_SERVICE_ENDPOINT", "http://localhost:8001/v1")
 
 st.title("Code Generation App")
 
@@ -19,12 +17,14 @@ if "messages" not in st.session_state:
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-llm = ChatOpenAI(base_url=args.model_url, 
+llm = ChatOpenAI(base_url=model_service, 
                  api_key="EMPTY",
                  streaming=True)
 
-prompt = ChatPromptTemplate.from_template("""You are helpful AI assistant to write a code for a given {input}""")
-
+# Define the Langchain chain
+prompt = ChatPromptTemplate.from_template("""You are an helpful code assistant that can help developer to code for a given {input}. 
+                                          Generate the code block at first, and explain the code at the end.
+                                          If the {input} is not in context, please ask more clarification is needed.""")
 chain = (
     {"input": RunnablePassthrough()}
     | prompt
@@ -34,7 +34,10 @@ chain = (
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").markdown(prompt)
-    response = chain.invoke(prompt)
+    
+    st_callback = StreamlitCallbackHandler(st.container())
+    response = chain.invoke(prompt, {"callbacks": [st_callback]})
+
     st.chat_message("assistant").markdown(response.content)    
     st.session_state.messages.append({"role": "assistant", "content": response.content})
     st.rerun()
