71a6cb37e654761b6fa94dc9bed60f36a9c3c530
Michael Clifford <mcliffor@redhat.com>
2024-03-13 09:52:59 -0400

    Add multimodal support (#45)
    * add multimodal support
    
    * put all params on one line
    
    * add gitattributes
    
    * Update image_understanding/builds/Containerfile
    
    Co-authored-by: Sébastien Han <seb@redhat.com>
    
    * fix repeat line in Containerfile
    
    ---------
    
    Co-authored-by: Sébastien Han <seb@redhat.com>

--- playground/run.sh
+++ playground/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -m llama_cpp.server --model ${MODEL_PATH} --host ${HOST:=0.0.0.0} --port ${PORT:=8001} --n_gpu_layers 0
+python -m llama_cpp.server --model ${MODEL_PATH} --host ${HOST:=0.0.0.0} --port ${PORT:=8001} --n_gpu_layers ${GPU_LAYERS:=0} --clip_model_path ${CLIP_MODEL_PATH:=None} --chat_format ${CHAT_FORMAT:="llama-2"}
