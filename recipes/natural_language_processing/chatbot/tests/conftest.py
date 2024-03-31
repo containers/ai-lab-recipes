e90c9991e3c7e030ab1a18e4d9e9cc5034462f11
Liora Milbaum <lmilbaum@redhat.com>
2024-03-31 14:32:28 +0300

    Spin Model Server in chatbot recipe
    Signed-off-by: Liora Milbaum <lmilbaum@redhat.com>

--- recipes/natural_language_processing/chatbot/tests/conftest.py
+++ recipes/natural_language_processing/chatbot/tests/conftest.py
@@ -1,6 +1,31 @@
 import pytest_container
 import os
+import pytest
+from selenium import webdriver
 
+
+MS = pytest_container.Container(
+        url=f"containers-storage:{os.environ['REGISTRY']}/model_servers",
+        volume_mounts=[
+            pytest_container.container.BindMount(
+                container_path="/locallm/models",
+                host_path="./",
+                flags=["ro"]
+            )
+        ],
+        extra_environment_variables={
+            "MODEL_PATH": "models/mistral-7b-instruct-v0.1.Q4_K_M.gguf",
+            "HOST": "0.0.0.0",
+            "PORT": "8001"
+        },
+        forwarded_ports=[
+            pytest_container.PortForwarding(
+                container_port=8001,
+                host_port=8001
+            )
+        ],
+        extra_launch_args=["--net=host"]
+    )
 
 CB = pytest_container.Container(
         url=f"containers-storage:{os.environ['REGISTRY']}/{os.environ['IMAGE_NAME']}",
@@ -21,3 +46,20 @@ def pytest_generate_tests(metafunc):
 
 def pytest_addoption(parser):
     pytest_container.add_logging_level_options(parser)
+
+@pytest.fixture
+def firefox_options(firefox_options):
+    firefox_options.headless = True
+    firefox_options.set_preference("webdriver.log.file", "/tmp/driver.log")
+    return firefox_options
+
+@pytest.fixture
+def custom_selenium(selenium, firefox_options):
+    selenium.webdriver = webdriver.Firefox(firefox_options=firefox_options)
+    yield selenium
+    selenium.webdriver.quit()
+
+@pytest.fixture
+def chrome_options(chrome_options):
+    chrome_options.add_argument("--headless")
+    return chrome_options
--- recipes/natural_language_processing/chatbot/tests/test_app.py
+++ recipes/natural_language_processing/chatbot/tests/test_app.py
@@ -10,3 +10,8 @@ def test_etc_os_release_present(auto_container: pytest_container.container.Conta
 @tenacity.retry(stop=tenacity.stop_after_attempt(5), wait=tenacity.wait_exponential())
 def test_alive(auto_container: pytest_container.container.ContainerData, host):
     host.run_expect([0],f"curl http://localhost:{auto_container.forwarded_ports[0].host_port}",).stdout.strip()
+
+def test_title(auto_container: pytest_container.container.ContainerData, selenium):
+    selenium.get(f"http://localhost:{auto_container.forwarded_ports[0].host_port}")
+    assert selenium.title == "Streamlit"
+
