938e2e0186b2c23c5cfbd57d0e226ca5f2694b9c
Liora Milbaum <lmilbaum@redhat.com>
2024-04-04 12:22:35 +0300

    Test Framework: integration tests
    Signed-off-by: Liora Milbaum <lmilbaum@redhat.com>

--- .github/scripts/provision.sh
+++ .github/scripts/provision.sh
@@ -2,7 +2,8 @@
 
 set -x
 
-dnf install -y podman wget
+dnf install -y podman wget \
+        https://s3.us-east-2.amazonaws.com/amazon-ssm-us-east-2/latest/linux_amd64/amazon-ssm-agent.rpm
 dnf clean all
 
 wget -P locallm/models https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q5_K_S.gguf
@@ -17,3 +18,6 @@ podman run -it \
         -p 8501:8501 \
         -e MODEL_SERVICE_ENDPOINT=http://10.88.0.1:8001/v1 \
         ghcr.io/containers/chatbot:latest
+
+# this file is sampled when the terraform apply is running
+touch /tmp/user_data_completed
