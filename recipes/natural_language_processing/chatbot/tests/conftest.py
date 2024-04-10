e2982c47aea995102a39580d5d790c86ced326fd
greg pereira <grpereir@redhat.com>
2024-04-09 17:30:38 -0700

    enabling tests on darwin
    Signed-off-by: greg pereira <grpereir@redhat.com>

--- recipes/natural_language_processing/chatbot/tests/conftest.py
+++ recipes/natural_language_processing/chatbot/tests/conftest.py
@@ -1,7 +1,27 @@
 import pytest
+from selenium import webdriver
+import platform
+import os
 
+if not os.environ["CHROMEDRIVER_PATH"]:
+    CHROMEDRIVER_PATH="../../chromedriver"
 
-@pytest.fixture
-def chrome_options(chrome_options):
+if platform.system() == "Darwin":
+    CHROME_PATH="../../Google\ Chrome.app"
+
+CHROMEDRIVER_PATH_ABS=os.path.abspath(CHROMEDRIVER_PATH)
+
+@pytest.fixture(scope="module")
+def chrome_driver(request):
+    chromedriver_path = '/path/to/chromedriver'
+    chrome_path = '/path/to/chrome'
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = chrome_path
+    driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
     chrome_options.add_argument("--headless")
-    return chrome_options
+    def teardown():
+        driver.quit()
+
+    request.addfinalizer(teardown)
+    return driver
