50a79740c1600bca967c3271b9799f203aab8c8e
Michael Clifford <mcliffor@redhat.com>
2024-05-15 18:51:06 -0400

    add interrupt to llamacpp (#494)
    Signed-off-by: Michael Clifford <mcliffor@redhat.com>

--- model_servers/llamacpp_python/src/run.sh
+++ model_servers/llamacpp_python/src/run.sh
@@ -5,7 +5,7 @@ if [ ${CONFIG_PATH} ] || [[ ${MODEL_PATH} && ${CONFIG_PATH} ]]; then
 fi
 
 if [ ${MODEL_PATH} ]; then
-    python -m llama_cpp.server --model ${MODEL_PATH} --host ${HOST:=0.0.0.0} --port ${PORT:=8001} --n_gpu_layers ${GPU_LAYERS:=0} --clip_model_path ${CLIP_MODEL_PATH:=None} --chat_format ${MODEL_CHAT_FORMAT:="llama-2"}
+    python -m llama_cpp.server --model ${MODEL_PATH} --host ${HOST:=0.0.0.0} --port ${PORT:=8001} --n_gpu_layers ${GPU_LAYERS:=0} --clip_model_path ${CLIP_MODEL_PATH:=None} --chat_format ${MODEL_CHAT_FORMAT:="llama-2"} --interrupt_requests ${INTERRUPT_REQUESTS:=False}
     exit 0
 fi
 
