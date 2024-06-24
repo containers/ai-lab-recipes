74de44f3aa2c2e87e99f741b5aa2568d198a2eb8
Enrique Belarte Luque <ebelarte@redhat.com>
2024-06-24 04:30:07 -0500

    Workaround for ilab script source
    Signed-off-by: Enrique Belarte Luque <ebelarte@redhat.com>

--- training/nvidia-bootc/add_ilab_wrapper.sh
+++ training/nvidia-bootc/add_ilab_wrapper.sh
@@ -4,5 +4,5 @@ LOCAL_ILAB_WRAPPER="run/.input/ilab"
 if [ -f "$LOCAL_ILAB_WRAPPER" ]; then
 	cp "$LOCAL_ILAB_WRAPPER" /usr/local/bin/ilab
 else
-	curl -o /usr/local/bin/ilab "https://raw.githubusercontent.com/enriquebelarte/ai-lab-recipes-upstream/main/training/ilab-wrapper/ilab"
+	curl -o /usr/local/bin/ilab "https://raw.githubusercontent.com/containers/ai-lab-recipes-upstream/main/training/ilab-wrapper/ilab"
 fi
