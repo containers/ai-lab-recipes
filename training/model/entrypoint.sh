5245d5b0083cd04eec238eb988df2cb1f79067d8
Javi Polo <jpolo@redhat.com>
2024-06-25 10:56:34 -0400

    Fix model-downloader container
    Signed-off-by: Javi Polo <jpolo@redhat.com>

--- training/model/entrypoint.sh
+++ training/model/entrypoint.sh
@@ -1,6 +1,7 @@
+set -x
 if [ -z "$HF_TOKEN" ]; then
     echo "Error. Please set your \$HF_TOKEN in env. Required to pull mixtral."
     exit 1
 fi
 
-huggingface-cli download --exclude "*.pt" --local-dir "/download/$(MODEL_REPO)" "$(MODEL_REPO)"
+huggingface-cli download --exclude "*.pt" --local-dir "/download/${MODEL_REPO}" "${MODEL_REPO}"
