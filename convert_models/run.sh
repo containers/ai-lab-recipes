f3fab081d8917d4b65851ece6c6f9464365933f6
Braxton Plaxco <bplaxco@redhat.com>
2025-02-13 13:26:20 -0500

    Fix issues with run script
    - Permission denied for builds done on systems with restrictive umask
    - New location of llama-quantize
    
    Signed-off-by: Braxton Plaxco <bplaxco@redhat.com>

--- convert_models/run.sh
+++ convert_models/run.sh
@@ -20,13 +20,13 @@ python download_huggingface.py --model $hf_model_url --token $hf_token
 python llama.cpp/examples/convert_legacy_llama.py /opt/app-root/src/converter/converted_models/$hf_model_url
 python llama.cpp/convert_hf_to_gguf.py /opt/app-root/src/converter/converted_models/$hf_model_url
 mkdir -p /opt/app-root/src/converter/converted_models/gguf/
-llama.cpp/llama-quantize /opt/app-root/src/converter/converted_models/$hf_model_url/ggml-model-f16.gguf /opt/app-root/src/converter/converted_models/gguf/$model_org-$model_name-${QUANTIZATION}.gguf ${QUANTIZATION}
+llama.cpp/build/bin/llama-quantize /opt/app-root/src/converter/converted_models/$hf_model_url/ggml-model-f16.gguf /opt/app-root/src/converter/converted_models/gguf/$model_org-$model_name-${QUANTIZATION}.gguf ${QUANTIZATION}
 rm -rf /opt/app-root/src/converter/converted_models/$model_org
 
 if [ $keep_orgi = "False" ]; then
     rm -rf /opt/app-root/src/converter/converted_models/cache
 fi
 
-echo "Converted and quantized model written to /opt/app-root/src/converter/converted_models/gguf/$model_org-$model_name.gguf" 
+echo "Converted and quantized model written to /opt/app-root/src/converter/converted_models/gguf/$model_org-$model_name.gguf"
 echo "$ ls /opt/app-root/src/converter/converted_models/gguf/"
 ls /opt/app-root/src/converter/converted_models/gguf/
